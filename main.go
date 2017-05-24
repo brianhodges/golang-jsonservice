aee23c26a6b8810681ca19611e17088e4524ecf2
Development <Development@Brian-N-Hodgess-MacBook.local>
2017-05-24 11:33:08 -0400

    add views into handlers

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
     "log"
     "os"
     "net/http"
+    "strings"
+    "regexp"
     "database/sql"
     "encoding/json"
     "golang-jsonservice/pkg/user"
@@ -18,33 +20,73 @@ func check(err error) {
     }
 }
 
-func main() {
-	fmt.Println("Running local server @ http://localhost:8080")
-    
-    http.HandleFunc("/users.json", func(w http.ResponseWriter, r *http.Request) {
-        url := r.FormValue("url")
-        if url == "" {    
-            db, err := sql.Open("postgres", os.Getenv("BASIC_APP_DATABASE_URL"))
-            check(err)
-            defer db.Close()
+func validIDJsonUrl(url string) bool {
+    Re := regexp.MustCompile(`^/users/\d.json$`)
+    return Re.MatchString(url)
+}
 
-            rows, err := db.Query("SELECT id, email, first_name, last_name FROM users")
-            users := []user.User{}
+//all users index view handler
+func index(w http.ResponseWriter, r *http.Request) {
+    url := r.FormValue("url")
+    if url == "" {    
+        db, err := sql.Open("postgres", os.Getenv("BASIC_APP_DATABASE_URL"))
+        check(err)
+        defer db.Close()
+
+        rows, err := db.Query("SELECT id, email, first_name, last_name FROM users")
+        users := []user.User{}
+        check(err)
+        for rows.Next() {
+            var u user.User
+            err = rows.Scan(&u.ID, &u.Email, &u.First_Name, &u.Last_Name)
             check(err)
-            for rows.Next() {
-                var u user.User
-                err = rows.Scan(&u.ID, &u.Email, &u.First_Name, &u.Last_Name)
-                check(err)
-                users = append(users, u)
-            }
-            
+            users = append(users, u)
+        }
+        
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        b, err := json.Marshal(users)
+        check(err)
+        fmt.Fprintf(w, string(b[:]))
+        return
+    }
+}
+
+//individual user show view handler
+func show(w http.ResponseWriter, r *http.Request) {
+    f := strings.Trim(r.URL.String(), "/users/")
+    id := strings.Trim(f, ".json")
+    if id != "" && validIDJsonUrl(r.URL.String()) {
+        db, err := sql.Open("postgres", os.Getenv("BASIC_APP_DATABASE_URL"))
+        check(err)
+        defer db.Close()
+    
+        rows, err := db.Query("SELECT id, email, first_name, last_name FROM users where id = " + id)
+        var user user.User
+        check(err)
+        for rows.Next() {
+            err = rows.Scan(&user.ID, &user.Email, &user.First_Name, &user.Last_Name)
+            check(err)
+        }
+        
+        if user.ID != 0 {
             w.Header().Set("Content-Type", "application/json; charset=utf-8")
-            b, err := json.Marshal(users)
+            b, err := json.Marshal(user)
             check(err)
             fmt.Fprintf(w, string(b[:]))
-            return
+        } else {
+            w.WriteHeader(http.StatusNotFound)
+            fmt.Fprint(w, "Error 204 - Content (User) Not Found")
         }
-    })
+        return
+    } else {
+        w.WriteHeader(http.StatusNotFound)
+        fmt.Fprint(w, "Error 404 - Page not found")
+    }
+}
 
+func main() {
+	fmt.Println("Running local server @ http://localhost:8080")
+    http.HandleFunc("/users.json", index)
+    http.HandleFunc("/users/", show)
     log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), nil))
 }
